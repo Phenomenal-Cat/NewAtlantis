c0744309803acbaa74756a78a20dbfdbeb20a9f0
Phenomenal-Cat <aidanmurphy1@hotmail.co.uk>
2021-03-16 11:14:08 -0400

    Front page cosmetic updates

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'recommonmark',
     'sphinx_inline_tabs',
     'sphinx_panels',
-    'sphinx_fontawesome',
+    # 'sphinx_fontawesome',
     # 'zot4rst.sphinx',
     'matplotlib.sphinxext.plot_directive',
 ]
